35535d237435abeb87f526f989876775025921c6
pabloleiton <pabloleitonrivera@gmail.com>
2023-03-03 11:59:04 -0600

    Add input exercises

---
+++ input_exercises.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welco0me to the band name generator")
+#2. Ask the user for the city that they grew up in.
+city = input("What is the name of the city where you grew up?\n")
+#3. Ask the user for the name of a pet.
+petname = input("What is you pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print(f"Your band name would be:\n {city} {petname}")
